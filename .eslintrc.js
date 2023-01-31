73891e5b56dc4d294237e04d69f1f350794f2f50
Sergio Perez <sergio-luis98@hotmail.com>
2023-01-31 17:55:20 -0500

    update

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
